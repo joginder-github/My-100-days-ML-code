b4f0e4dc6e5956c9fa3ac54a1ce504bb585aa0ef
joginder-github <40789188+joginder-github@users.noreply.github.com>
2018-08-03 21:33:09 +0530

    declaration_of_variable

---
+++ day-0/variable.py
@@ -0,0 +1,11 @@
+name = "Joginder Pal"
+firstName = "Joginder"
+lastName = "Pal"
+print(name)
+print(firstName+" python "+lastName)
+
+#  how to trace index
+print(name.index("J"))
+
+# Using replace function
+print(name.replace("Joginder", "Pal"))
